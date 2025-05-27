a21d87962ccfdb11dba2622924fa63f40b2c133e
blacksmith69-glitch <cryptowitharyancourselink@gmail.com>
2025-05-27 19:45:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function copyToClipboard(id) {
-  const el = document.getElementById(id);
-  navigator.clipboard.writeText(el.innerText).then(() => {
-    const notif = document.getElementById("copied-" + id);
-    notif.style.display = "block";
-    setTimeout(() => notif.style.display = "none", 1500);
-  });
-}
